443005d62c2b6f2630be2cfeae3ad4b88eea6900
Patrick Walters <wpwalters@gmail.com>
2023-06-08 21:38:57 -0400

    removed constants.py

--- solubility/constants.py
+++
@@ -1,7 +0,0 @@
-import chembl_downloader
-import click
-import pystow
-
-SFI_MODULE = pystow.module("PatWalters", "sfi")
-
-chembl_version_option = click.option("--chembl-version", default=chembl_downloader.latest)
